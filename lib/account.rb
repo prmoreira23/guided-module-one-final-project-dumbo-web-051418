b2e7a8becdf3fa8968ecab03e0d0de361c841d95
prmoreira23 <prm.gredes@gmail.com>
2018-05-30 09:56:42 -0400

    Added authentication, deposit and withdraw to Account and Balance.

--- lib/account.rb
+++ lib/account.rb
@@ -1,4 +1,22 @@
 class Account < ActiveRecord::Base
   has_many :balances
   has_many :coins, through: :balances
+  validates :username, unique: true
+
+  # Returns an instance of Account if authentication is successful
+  # returns nil otherwise.
+  def authenticate(password)
+    self.password == password ? self : nil
+  end
+
+  # Withdraw a certain amount from a balance
+  # Can raise a NotEnoughFunds exception or returns the new balance if successful
+  def withdraw(balance, amount)
+    balance.withdraw(ammount)
+  end
+
+
+  def deposit(balance, amount)
+    balance.deposit(amount)
+  end
 end
--- lib/balance.rb
+++ lib/balance.rb
@@ -1,4 +1,22 @@
 class Balance < ActiveRecord::Base
   belongs_to :account
   belongs_to :coin
+
+  # Returns current balance if transaction is successful,
+  # raises an exception otherwise
+  def withdraw(amount)
+    if self.amount >= amount
+      self.amount -= amount
+    else
+      raise NotEnoughFunds.new('There is not enough funds to process this transaction.')
+    end
+  end
+
+  # Adds a deposit amount to the balance amount
+  def deposit(amount)
+    self.amount += amount
+  end
+
+  class NotEnoughFunds < StandardError
+  end
 end
